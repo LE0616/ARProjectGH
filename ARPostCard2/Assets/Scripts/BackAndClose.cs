4bbc5f0f50f4257271e0681eb1db1f3802bf0804
LE (Lee) <lm61613@gmail.com>
2018-11-11 14:57:24 -0500

    Initiating with GitHub
    PostCard AR 1 & 2 files included....ARMap coming soon

---
+++ ARPostCard2/Assets/Scripts/BackAndClose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackAndClose : MonoBehaviour {
+
+	public void backbtn()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void exitbtn()
+    {
+        Application.Quit();
+        Debug.Log("Exit button pressed");
+    }
+
+}
---
+++ ARPostCard2/Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour {
+
+	public void greetingCard()
+    {
+        SceneManager.LoadScene("ARPostCardScene");
+    }
+
+    public void rotateCube()
+    {
+        SceneManager.LoadScene("RotateCube");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit Button pressed");
+    }
+}
